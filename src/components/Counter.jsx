db26b9408977e7f76b83f69c00f2ffb38c85b2c7
Craig Clayton <craig__clayton@outlook.com>
2025-01-22 11:18:27 +0000

    make increment/decrement and reset function in Counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+  const increment = () => setCount((c) => c + 1);
+  const decrement = () => setCount((c) => c - 1);
+  const reset = () => setCount(0);
   return (
     <div style={{ margin: 'auto' }}>
       <h3>{count}</h3>
-      <button onClick={() => setCount((c) => c + 1)}>+1</button>
-      <button onClick={() => setCount((c) => c - 1)}>-1</button>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 };
